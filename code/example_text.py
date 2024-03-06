f71160b78bc6341c79f0b8b6c67386f2686476ab
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-03-06 10:43:29 -0500

    chore()[]: remove files

--- code/example_text.py
+++
@@ -1,9 +0,0 @@
-Dogs, known for their unwavering loyalty and affection, have been cherished companions to humans for centuries. These four-legged friends come in various breeds, each with its own unique traits and personalities. From playful puppies bounding with energy to wise old dogs offering comfort, the bond between humans and dogs is a testament to the enduring companionship these animals bring into our lives.
-
-
-Pangolins, often referred to as the world's only scaly mammal, face severe threats to their survival due to illegal wildlife trafficking and habitat loss. These elusive creatures play a crucial role in their ecosystems by controlling insect populations, yet they are among the most trafficked mammals globally. Conservation efforts are underway to protect pangolins from the brink of extinction, emphasizing the importance of raising awareness about these unique and endangered species.
-
-
-Exploring the realms of unlikely animal friendships, the story of perritos (little dogs) and pangolins unveils an unexpected bond. In certain conservation sanctuaries, canines are trained to help protect pangolins from poachers. These specially trained dogs use their keen sense of smell to locate trafficked pangolins, aiding in conservation efforts to rehabilitate and release these scaly mammals back into their natural habitats. This heartwarming collaboration showcases the positive impact that interspecies conn
-
-
